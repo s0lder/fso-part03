b9706605cd7e1b5c37a93b94305c0a7a144991a4
sldr <sldr@laptop>
2024-08-24 18:02:49 +0300

    uibuild

--- index.js
+++ index.js
@@ -14,31 +14,6 @@ morgan.token('person', (request, response) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
-/*
-let persons = [
-  {
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-*/
-
 let persons = []
 
 const handleError = (response, status, msg) => response.status(status).json({ error: msg })
